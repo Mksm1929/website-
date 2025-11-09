6dd3c55463c85c31023deeff3d88e4f37c8e91ba
Maksim <maksim.varnin@mail.ru>
2025-11-09 04:22:35 +0300

    адаптировал блок 'о себе'

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from "./components/Header/Header";
 import { Router } from "./provider/router";
 import { Footer } from "./components/Footer/Footer";
 import "./AppCommon.css";
+import { TextWriter } from "./components/TextWriter";
 
 
 function App() {
@@ -12,6 +13,17 @@ function App() {
       <Header />
       <WelcomePage />
       <div className="app-content">
+              <div className="container-welcome-p">
+                <TextWriter
+                  text={`Начинающий Frontend-разработчик с уверенными знаниями\r\n
+                  HTML, CSS, JavaScript и базовым опытом работы с React.\r\n
+                  Читаю профильную литературу, например "Чистый код".\n
+                  Быстро обучаюсь, умею работать с Git.\n
+                  Ищу первую работу в команде,\n
+                  где смогу развиваться и приносить пользу.`}
+                  speed={25}
+                />
+              </div>
         <Router />
         <div className="app-footer">
           <Footer />
--- src/components/WelcomePage/WelcomePage.tsx
+++ src/components/WelcomePage/WelcomePage.tsx
@@ -6,20 +6,9 @@ import { TextWriter } from "../TextWriter";
 export const WelcomePage = () => {
   return (
     <div className="container-welcome">
-      <div className="container-welcome-p">
-        <TextWriter
-          text={`Начинающий Frontend-разработчик с уверенными знаниями\r\n
-          HTML, CSS, JavaScript и базовым опытом работы с React.\r\n
-          Читаю профильную литературу, например "Чистый код".\n
-          Быстро обучаюсь, умею работать с Git.\n
-          Ищу первую работу в команде,\n
-          где смогу развиваться и приносить пользу.`}
-          speed={25}
-        />
-      </div>
       <div className="welcome">
 
-        <div className="welcome-welcome">
+        <div className="welcome-frontend">
           <span>Frontend</span>
         </div>
         <div className="welcome-container">
