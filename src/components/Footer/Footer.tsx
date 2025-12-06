f33ad1b661d390680e2b0db5e1e6d2258d50438f
Maksim <maksim.varnin@mail.ru>
2025-12-07 00:51:10 +0300

    добавил ссылку на телеграм

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import { GithubOutlined, WhatsAppOutlined } from "@ant-design/icons";
 import "./Footer.css";
+import Telegram from "../../assets/icons/telegram.png";
 
 
 export const Footer = () => {
@@ -10,8 +11,11 @@ export const Footer = () => {
             <a target="_blank" href="https://github.com/Mksm1929">
             <GithubOutlined className="cursor-pointer footer-contact-icon color-white" />
             </a>
-            <a target="_blank" href="https://wa.me/89995389074">
+            <a target="_blank" href="https://wa.me/+79995389074">
             <WhatsAppOutlined className="cursor-pointer footer-contact-icon color-green" />
+            </a>
+            <a target="_blank" href="https://t.me/maksim_varnin">
+            <img src={Telegram} className="cursor-pointer footer-contact-icon-telegram" />
             </a>
         </div>
     )
